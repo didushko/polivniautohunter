0bb6dd1793d42877fb8aa822ae3f2f5842ace463
Denis Didushko <denis.didushko@gmail.com>
2025-03-17 18:02:02 +0100

    add home responce

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,43 @@
-import * as http from "http";
-import { processAllTrackings, sendTestMessage } from "./services/polav-service";
-import axios from "axios";
-import { Bot } from "./app";
+import * as http from 'http';
+import { processAllTrackings, sendTestMessage } from './services/polav-service';
+import axios from 'axios';
+import { Bot } from './app';
 
 export function startServer(bot: Bot) {
   const server = http.createServer(async (req, res) => {
     try {
-      if (req.method === "GET" && req.url === "/send/all") {
-        console.log("Receive send all comand");
+      if (req.method === 'GET' && req.url === '/send/all') {
+        console.log('Receive send all comand');
         await processAllTrackings(bot.bot);
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end("All messages sended");
-      } else if (req.method === "GET" && req.url === "/send/test") {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('All messages sended');
+      } else if (req.method === 'GET' && req.url === '/send/test') {
         await sendTestMessage(bot.bot);
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end(JSON.stringify({ message: "Test sended" }));
-      } else if (req.method === "GET" && req.url?.startsWith("/date")) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(JSON.stringify({ message: 'Test sended' }));
+      } else if (req.method === 'GET' && req.url?.startsWith('/date')) {
         const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
-        const date = parsedUrl.searchParams.get("q");
+        const date = parsedUrl.searchParams.get('q');
         if (date) {
-          res.writeHead(200, { "Content-Type": "text/plain" });
-          res.end(JSON.stringify({ message: `Date: ${date} ${Date.parse(date)}}` }));
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(
+            JSON.stringify({ message: `Date: ${date} ${Date.parse(date)}}` })
+          );
         } else {
-          res.writeHead(400, { "Content-Type": "text/plain" });
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
           res.end(JSON.stringify({ error: "Missing 'q' parameter" }));
         }
+      } else if (req.method === 'GET' && req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('PolovniAutoHunter_bot work now!');
       } else {
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end("Not Found");
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
       }
     } catch (e) {
       console.log(e);
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Error");
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Error');
     }
   });
 
@@ -45,9 +50,9 @@ export function startServer(bot: Bot) {
 }
 
 function selfReq() {
-  const p1 = "https://poliv";
-  const p2 = "niautohunter.onren";
-  const p3 = "der.com";
+  const p1 = 'https://poliv';
+  const p2 = 'niautohunter.onren';
+  const p3 = 'der.com';
   const url = p1 + p2 + p3;
 
   setInterval(async () => {
